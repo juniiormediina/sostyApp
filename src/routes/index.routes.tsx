e8b7f630fc4cbe04e21433a9042dd59526ba04f5
Junior Medina <junmedin@bancolombia.com.co>
2022-11-06 12:06:10 -0500

    Se genera implementacion del nuevo componente de inversiones

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -4,6 +4,7 @@ import {Login} from '../screens/Login/Login.component';
 import {useRoutes} from './hooks/useIndex.routes';
 import {ForgotPassword} from '../screens/Login/ForgotPassword.component';
 import {CreateAccount} from '../screens/Login/CreateAccount.component';
+import Investments from '../screens/Investments/Investments.component';
 
 export const Routes = () => {
   const {Stack} = useRoutes();
@@ -13,6 +14,7 @@ export const Routes = () => {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="CreateAccount" component={CreateAccount} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="Investments" component={Investments} />
       </Stack.Navigator>
     </NavigationContainer>
   );
