79c6f75ebbb2d434de67946989573124b279714a
Eder Arias <ederarias001@gmail.com>
2022-11-08 15:36:41 -0500

    help

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -6,17 +6,19 @@ import {ForgotPassword} from '../screens/Login/ForgotPassword.component';
 import {CreateAccount} from '../screens/Login/CreateAccount.component';
 import Investments from '../screens/Investments/Investments.component';
 import { Menu } from '../components/Menu.component';
+import { Help } from '../screens/Help/Help.component';
 
 export const Routes = () => {
   const {Stack} = useRoutes();
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Menu" component={Menu} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="CreateAccount" component={CreateAccount} />
         <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         <Stack.Screen name="Investments" component={Investments} />
+        <Stack.Screen name="Help" component={Help} />
+        <Stack.Screen name="Menu" component={Menu} />
       </Stack.Navigator>
     </NavigationContainer>
   );
